012c7eb7845f4e98b371af54e760232792668963
Dustin Walker! <dustinwalkerart@gmail.com>
2022-02-02 22:14:21 +0900

    fixes jump movement

--- jump.gd
+++ jump.gd
@@ -4,30 +4,39 @@ var stateMachine = null;
 var myAnim;
 var myBody;
 var myHitBox;
-var preJump = 3;
-var jumpFrames = 30;
+var speed = 0;
+var speedGain = 3;
 
 var vector = Vector2();
 func enter(body, animator): 
 	myBody = body;
 	myAnim = animator;
 	myAnim.play('idle');
-	preJump = 3;
-	jumpFrames = 30;
-	#myBody.move_and_slide(Vector2(0, 200))
 	stateMachine.velocity.y = stateMachine.jumpVelocity;
+	speed = 0;
 	
 func exit(): 
 	pass
 
 func update(delta):
-	if Input.is_action_pressed('right'):
-		get_node('../../Sprite').set_flip_h(false);
-		stateMachine.velocity.x = 30;
-	elif Input.is_action_pressed('left'):
-		get_node('../../Sprite').set_flip_h(true);
-		stateMachine.velocity.x = -30;
+	if Input.is_action_pressed('right') && Input.is_action_pressed('left'):
+		# this conditional checks to make sure we aren't holding two keys at the same time.
+		pass
+	else:
+		if Input.is_action_pressed('right'):
+			speed += speedGain;
+			get_node('../../Sprite').set_flip_h(false);
+			if speed <= stateMachine.maxSpeed: 
+				stateMachine.velocity.x = speed;
+		elif Input.is_action_pressed('left'):
+			speed += speedGain;
+			get_node('../../Sprite').set_flip_h(true);
+			if speed <= stateMachine.maxSpeed: 
+				stateMachine.velocity.x = -speed;
 	if myBody.is_on_floor(): 
 		stateMachine.transitionTo('idle');
+		
+	if Input.is_action_just_released('right') || Input.is_action_just_released('left'):
+		speed = 0;
 	pass 
 
